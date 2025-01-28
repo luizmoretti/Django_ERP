430313940dfdcfe6fbcf6eb0859fcb2fde516493
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-01-28 11:25:34 -0500

    feat(employeers): update list_display to use 'user_type' instead of 'type'

--- apps/companies/employeers/admin.py
+++ apps/companies/employeers/admin.py
@@ -4,7 +4,7 @@ from .models import Employeer
 
 @admin.register(Employeer)
 class EmployeerAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'user__type','age', 'created_at', 'updated_at', 'created_by', 'updated_by', 'companie']
+    list_display = ['id', 'user', 'user__user_type','age', 'created_at', 'updated_at', 'created_by', 'updated_by', 'companie']
     list_filter = ['created_at', 'updated_at', 'created_by', 'updated_by', 'companie']
     search_fields = ['user__first_name', 'user__last_name', 'user__email', 'companie__name']
     readonly_fields = ['id', 'created_at', 'updated_at', 'created_by', 'updated_by']
