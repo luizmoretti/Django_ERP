6776be6039dc5f46b93e5bba5773951932396bb0
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-01-29 10:36:23 -0500

    Delete(delivery_os) Deleted app `delivery_os` and replaced with `load_order` to reflect correct name of what the app is about

--- apps/inventory/load_order/apps.py
+++ apps/inventory/load_order/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DeliveryOsConfig(AppConfig):
+class LoadOrderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.inventory.delivery_os'
+    name = 'apps.inventory.load_order'
