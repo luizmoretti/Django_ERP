da626123c98e49622dfe49eba292f1eddfa4bd90
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-02-03 15:03:06 -0500

    :bug: Fix: Replaces the fields 'get_created_by_full_name' and 'get_updated_by_full_name' with 'created_by' and 'updated_by' in the Outflow admin display

--- apps/inventory/outflows/admin.py
+++ apps/inventory/outflows/admin.py
@@ -12,16 +12,11 @@ class OutflowItemsInline(admin.TabularInline):
 @admin.register(Outflow)
 class OutflowAdmin(admin.ModelAdmin):
     inlines = [OutflowItemsInline]
-    list_display = ('get_origin_address', 'get_destiny_address', 'created_at', 'updated_at', 'get_created_by_full_name', 'get_updated_by_full_name')
+    list_display = ('get_origin_address', 'get_destiny_address', 'created_at', 'updated_at', 'created_by', 'updated_by')
     list_filter = ('origin', 'destiny', 'created_at', 'updated_at')
-    search_fields = ('origin__name', 'destiny_full_name')
+    search_fields = ('origin__name', 'destiny__name')
     readonly_fields = ('id', 'companie', 'created_at', 'updated_at', 'created_by', 'updated_by')
     
-    def get_created_by_full_name(self, obj):
-        if obj.created_by:
-            return f"{obj.created_by.first_name} {obj.created_by.last_name}".strip()
-        return "-"
-    get_created_by_full_name.short_description = "Created By"
     
     def get_origin_address(self, obj):
         if obj.origin:
@@ -33,12 +28,6 @@ class OutflowAdmin(admin.ModelAdmin):
     def get_destiny_address(self, obj):
         if obj.destiny:
             if obj.destiny.another_shipping_address:
-                return obj.destiny.customer_project_address_customer.first()
+                return obj.destiny.customer_project_address.first()
         return "-"
     get_destiny_address.short_description = "Destiny"
-    
-    def get_updated_by_full_name(self, obj):
-        if obj.updated_by:
-            return f"{obj.updated_by.first_name} {obj.updated_by.last_name}".strip()
-        return "-"
-    get_updated_by_full_name.short_description = "Updated By"
