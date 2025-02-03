2ddd1767f4631f10dff23ac43562fee3bdf047ce
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-02-03 14:47:14 -0500

    :bug: Fix: Simplifies the display and search of the 'created_by' and 'updated_by' fields in the supplier admin

--- apps/inventory/supplier/admin.py
+++ apps/inventory/supplier/admin.py
@@ -3,19 +3,7 @@ from .models import Supplier
 
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
-    list_display = ('name', 'tax_number', 'created_at', 'updated_at', 'get_created_by_full_name', 'get_updated_by_full_name')
-    list_filter = ('name', 'created_at', 'updated_at', 'created_by', 'updated_by')
-    search_fields = ('name', 'tax_number', 'created_by__first_name', 'created_by__last_name', 'updated_by__first_name', 'updated_by__last_name')
-    readonly_fields = ('id', 'companie', 'created_at', 'updated_at', 'created_by', 'updated_by')
-    
-    def get_created_by_full_name(self, obj):
-        if obj.created_by:
-            return f"{obj.created_by.first_name} {obj.created_by.last_name}".strip()
-        return "-"
-    get_created_by_full_name.short_description = "Created By"
-    
-    def get_updated_by_full_name(self, obj):
-        if obj.updated_by:
-            return f"{obj.updated_by.first_name} {obj.updated_by.last_name}".strip()
-        return "-"
-    get_updated_by_full_name.short_description = "Updated By"
+    list_display = ('name', 'tax_number', 'created_at', 'updated_at', 'created_by', 'updated_by')
+    list_filter = ('name', 'created_at', 'updated_at')
+    search_fields = ('name', 'tax_number', 'created_by__name', 'updated_by__name')
+    readonly_fields = ('id', 'created_at', 'updated_at', 'created_by', 'updated_by', 'companie')
