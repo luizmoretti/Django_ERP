0d0fa7cb66f8b51d0dc488a779e33b76939c3be0
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-02-03 14:48:04 -0500

    :bug: Fix: Replaces the 'get_created_by_full_name' and 'get_updated_by_full_name' fields with 'created_by' and 'updated_by' in the Transfer admin display

--- apps/inventory/transfer/admin.py
+++ apps/inventory/transfer/admin.py
@@ -12,20 +12,7 @@ class TransferItemsInline(admin.TabularInline):
 @admin.register(Transfer)
 class TransferAdmin(admin.ModelAdmin):
     inlines = [TransferItemsInline]
-    list_display = ('origin', 'destiny', 'created_at', 'updated_at', 'get_created_by_full_name', 'get_updated_by_full_name')
+    list_display = ('origin', 'destiny', 'created_at', 'updated_at', 'created_by', 'updated_by')
     list_filter = ('origin', 'destiny', 'created_at', 'updated_at')
     search_fields = ('origin__name', 'destiny__name')
     readonly_fields = ('id', 'companie', 'created_at', 'updated_at', 'created_by', 'updated_by')
-    
-    
-    def get_created_by_full_name(self, obj):
-        if obj.created_by:
-            return f"{obj.created_by.first_name} {obj.created_by.last_name}".strip()
-        return "-"
-    get_created_by_full_name.short_description = "Created By"
-    
-    def get_updated_by_full_name(self, obj):
-        if obj.updated_by:
-            return f"{obj.updated_by.first_name} {obj.updated_by.last_name}".strip()
-        return "-"
-    get_updated_by_full_name.short_description = "Updated By"
