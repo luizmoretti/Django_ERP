f4aac5dc2f7bfa3f3f3121af2c6dcbb4c2ea8826
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-02-03 10:27:39 -0500

    :bug: Fix: Corrigido erro `GET`  em Django Admin no modulo `load_order`

--- apps/inventory/load_order/admin.py
+++ apps/inventory/load_order/admin.py
@@ -11,20 +11,20 @@ class LoadOrderItemInline(admin.TabularInline):
 @admin.register(LoadOrder)
 class LoadOrderAdmin(admin.ModelAdmin):
     inlines = [LoadOrderItemInline]
-    list_display = ('order_number', 'load_date', 'created_at', 'updated_at', 'get_created_by_full_name', 'get_updated_by_full_name')
+    list_display = ('order_number', 'load_date', 'created_at', 'updated_at', 'created_by', 'updated_by')
     list_filter = ('created_at', 'updated_at')
-    search_fields = ('order_number', 'created_by__first_name', 'created_by__last_name', 'updated_by__first_name', 'updated_by__last_name')
+    search_fields = ('order_number', 'created_by__name', 'updated_by__name')
     readonly_fields = ('id', 'order_number', 'companie', 'created_at', 'updated_at', 'created_by', 'updated_by')
     
     
-    def get_created_by_full_name(self, obj):
-        if obj.created_by:
-            return f"{obj.created_by.first_name} {obj.created_by.last_name}".strip()
-        return "-"
-    get_created_by_full_name.short_description = "Created By"
+    # def get_created_by_full_name(self, obj):
+    #     if obj.created_by:
+    #         return f"{obj.created_by.first_name} {obj.created_by.last_name}".strip()
+    #     return "-"
+    # get_created_by_full_name.short_description = "Created By"
     
-    def get_updated_by_full_name(self, obj):
-        if obj.updated_by:
-            return f"{obj.updated_by.first_name} {obj.updated_by.last_name}".strip()
-        return "-"
-    get_updated_by_full_name.short_description = "Updated By"
+    # def get_updated_by_full_name(self, obj):
+    #     if obj.updated_by:
+    #         return f"{obj.updated_by.first_name} {obj.updated_by.last_name}".strip()
+    #     return "-"
+    # get_updated_by_full_name.short_description = "Updated By"
