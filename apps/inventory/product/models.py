2ae4e9723ea0020bbd542e367096110920ea2c96
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-02-04 13:10:32 -0500

    :sparkles: Feat: Adds minimum and maximum quantity fields to the product template

--- apps/inventory/product/models.py
+++ apps/inventory/product/models.py
@@ -23,6 +23,9 @@ class Product(BaseModel):
     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True, related_name='products', help_text='The brand of the product')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products', help_text='The category of the product')
     
+    # Trash Hold Fields for warehouse low_stock notification
+    min_quantity = models.PositiveIntegerField(default=0, help_text='The minimum quantity of the product that should be in stock')
+    max_quantity = models.PositiveIntegerField(default=0, help_text='The maximum quantity of the product that should be in stock')
     class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
