13c40dfb3412b7060df761b85c4286f70b14be35
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-02-05 14:15:12 -0500

    Feat: Updates Celery's task schedule to run every 24 hours

--- core/celery.py
+++ core/celery.py
@@ -16,23 +16,23 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-# # Configure periodic tasks
+# Configure periodic tasks
 app.conf.beat_schedule = {
     'generate-daily-inflow-report': {
-        'task': 'inflows.tasks.generate_daily_inflow_report',
-        'schedule': crontab(hour=13, minute=8),  # Run at 20:00 every day
-        'options': {'queue': 'reports'},  # Run on the 'reports' queue
+        'task': 'generate_daily_inflow_report',
+        'schedule': crontab(hour='*/24'),  # Runs every 24 hours
+        'options': {'queue': 'reports'},
     },
     'check-low-stock': {
         'task': 'check_low_stock',
-        'schedule': crontab(hour=13, minute=4), # Run at 07:00 every day
+        'schedule': crontab(hour='*/24'),  # Runs every 24 hours
         'options': {'queue': 'warehouse'},
     },
-    'check-specific-product': {
-        'task': 'check_specific_product',
-        'schedule': crontab(hour=7, minute=0), # Run at 07:00 every day
-        'options': {'queue': 'warehouse'},
-    }
+    # 'check-specific-product': {
+        # 'task': 'check_specific_product',
+        # 'schedule': crontab(minute='*/1'),  # Runs every 1 minute
+        # 'options': {'queue': 'warehouse'},
+    # }
 }
 
 @app.task(bind=True, ignore_result=True)
