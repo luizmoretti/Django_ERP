08c302bf807ef57291850c61a51ada6df8226553
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-02-22 14:54:28 -0500

    :recycle: Refactor: Renames product routes to follow consistent naming conventions

--- BackEnd/apps/inventory/product/urls.py
+++ BackEnd/apps/inventory/product/urls.py
@@ -5,12 +5,12 @@ app_name = 'product'
 
 urlpatterns = [
     # Product URLs
-    path('', views.ProductListView.as_view(), name='product-list'),
-    path('create/', views.ProductCreateView.as_view(), name='product-create'),
-    path('retrieve/<uuid:pk>/', views.ProductRetrieveView.as_view(), name='product-detail'),
-    path('update/<uuid:pk>/', views.ProductUpdateView.as_view(), name='product-update'),
-    path('delete/<uuid:pk>/', views.ProductDestroyView.as_view(), name='product-delete'),
+    path('', views.ProductListView.as_view(), name='list_products'),
+    path('create/', views.ProductCreateView.as_view(), name='create_product'),
+    path('retrieve/<uuid:pk>/', views.ProductRetrieveView.as_view(), name='retrieve_product'),
+    path('update/<uuid:pk>/', views.ProductUpdateView.as_view(), name='update_product'),
+    path('delete/<uuid:pk>/', views.ProductDestroyView.as_view(), name='delete_product'),
     
     # Home Depot Actions
-    path('home-depot/', views.HomeDepotActionsView.as_view(), name='home-depot-actions'),
+    path('home-depot/', views.HomeDepotActionsView.as_view(), name='hd_actions'),
 ]
