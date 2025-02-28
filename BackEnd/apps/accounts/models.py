a6b80a8adb195ae72f5f6a30389af07f9386985a
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-02-28 10:27:52 -0500

    :recycle: Refactor: Translating error messages from `pt-br` to `en-us`

--- BackEnd/apps/accounts/models.py
+++ BackEnd/apps/accounts/models.py
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
     """
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('O email é obrigatório')
+            raise ValueError('Email is mandatory')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -26,9 +26,9 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_active', True)
         
         if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser deve ter is_staff=True.')
+            raise ValueError('The superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser deve ter is_superuser=True.')
+            raise ValueError('The superuser must have is_superuser=True.')
         
         return self.create_user(email, password, **extra_fields)
 
