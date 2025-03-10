4ee17e4ce721246a596150e2a13b6fb8d7e9ddf9
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-03-10 15:15:53 -0400

    excluding deliveries module

--- BackEnd/apps/deliveries/tracking/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/delivery/tracking/(?P<delivery_id>[0-9a-f-]+)/$', consumers.DeliveryTrackingConsumer.as_asgi()),
-]
