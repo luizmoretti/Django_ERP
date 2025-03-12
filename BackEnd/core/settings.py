6fb51a8e966e15627dc5e7419530c8434f89d5a3
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-03-12 09:36:23 -0400

    feat: Adds basic authentication to the REST framework

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -531,6 +531,7 @@ if not os.path.exists('logs'):
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
