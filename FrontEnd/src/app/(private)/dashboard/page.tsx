1eb73592bbb5d458935ac2c471544ac63c620705
Erick Eduardo <erickdudu12@hotmail.com>
2025-03-18 15:44:25 -0300

    feat: Add DateRange and Dashboard

--- FrontEnd/src/app/(private)/dashboard/page.tsx
+++ FrontEnd/src/app/(private)/dashboard/page.tsx
@@ -2,12 +2,49 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useSidebar } from "@/components/sidebar/sidebarcontext";
 import ChartOverview from "@/components/chart";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+
 
 export default function Dashboard() {
   const {isSidebarVisible} =  useSidebar();
 
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+  const handleFilter = ()=> {
+    console.log("Filtrando de", startDate, "até", endDate);
+  };
+
   return (
     <main className={`p-4 transition-margin duration-300 ease-in-out ${isSidebarVisible ? "ml-64" : "ml-0"}`} style={{marginTop: "3.5rem"}}>
+      <section className="flex items-center gap-2 mb-4">
+        <DatePicker
+        selected={startDate}
+        onChange={(date)=> setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+        className="border p-2 rounded-md w-40"
+        placeholderText="Start Date"
+        />
+        <DatePicker
+        selected={endDate}
+        onChange={(date)=> setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        className="border p-2 rounded-md w-40"
+        placeholderText="End Date"
+        />
+        <button
+        onClick={handleFilter}
+        className="bg-blue-700 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+        >
+          Filter
+        </button>
+      </section>
+
       <section className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <Card>
             <CardHeader>
