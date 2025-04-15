2f824c461054051e8fa5802b8df758cee91ff914
Luiz Moretti <luizantoniomoretti@icloud.com>
2025-04-15 14:07:03 -0400

    refactor: correctly pass the name of the module so to system identifies

--- BackEnd/apps/scheduller/apps.py
+++ BackEnd/apps/scheduller/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SchedullerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scheduller'
+    name = 'apps.scheduller'
